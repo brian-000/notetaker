c20b2aacb1a19c91266d3cea36d0d22d819b1601
Brain <ortiz_brian323@yahoo.com>
2022-05-20 21:51:31 -0700

    added html routes, initialized server

---
+++ routes/apiRoutes/notesRoute.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+//const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { notes } = require('../../db/db');
+
+
+
+module.exports = router;
