0e06750731baa57f610c55c2bc1c3103877c22f3
Brain <ortiz_brian323@yahoo.com>
2022-05-23 20:42:43 -0700

    added get/post routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,21 +1,32 @@
 //middleware
 const router = require('express').Router();
-// router.use(require('./zookeepersRoutes'));
 const { filterByQuery } = require('../../lib/notes');
-const { notes } = require('../../db/db');
+const notes = require('../../db/db.json');
 // Require file system
 const fs = require("fs");
 // Require path - this allows us to access to path
 const path = require("path");
 
+router.post("/notes", (req, res) => {
+  var newNote = {
+    title: req.body.title,
+    text: req.body.text
+  }
+
+  notes.push(newNote);
+
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notes)
+  );
+  res.json(newNote);
+
+});
 
 router.get("/notes", (req, res) => {
-    // Set results equal to notes
-  
-    // Set response equal to JSON(results)
-    res.sendFile(path.join(__dirname, "../../db/db.json"));
-  });
+  res.sendFile(path.join(__dirname, "../../db/db.json"));
 
+});
 module.exports = router;
 
 
