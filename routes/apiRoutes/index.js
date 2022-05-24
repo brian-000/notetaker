ddcdb5bfdac797b3c69c61453e4377f68eb80238
Brain <ortiz_brian323@yahoo.com>
2022-05-24 01:57:18 -0700

    added bonus delete functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,12 +1,17 @@
 //middleware
 const router = require('express').Router();
-var uniqid = require('uniqid'); 
+var uniqid = require('uniqid');
 const { filterByQuery } = require('../../lib/notes');
 const notes = require('../../db/db.json');
 // Require file system
 const fs = require("fs");
 // Require path - this allows us to access to path
 const path = require("path");
+
+function findById(id, notessArray) {
+  const result = notessArray.filter(notes => notes.id === id)[0];
+  return result;
+}
 
 router.post("/notes", (req, res) => {
   var newNote = {
@@ -29,6 +34,23 @@ router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../../db/db.json"));
 
 });
+
+router.delete("/notes/:id", (req, res) => {
+  result = findById(req.params.id, notes);
+  notes.forEach(element => {
+    if(result.id == element.id){
+      // notes.splice(element.id, 1);
+      notes.splice(element,1);
+    }
+  });
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notes)
+  );
+  res.json(notes);
+});
+
+
 module.exports = router;
 
 
